70ab162b1054b0f189021d5aa43b5ddcc8c6d054
jonas.voland <jonas.voland@students.ffhs.ch>
2024-04-06 03:18:00 +0200

    added daisyUI

--- src/pages/PageHome.tsx
+++ src/pages/PageHome.tsx
@@ -2,6 +2,7 @@ const PageHome = () => {
     return (
         <div className="grid gap-4">
             Hello from Page Home
+            <button className="btn">Button</button>
         </div>
     )
 }
