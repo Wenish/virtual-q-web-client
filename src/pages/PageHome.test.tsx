97e9124298ce36fa61b97e87c29ef8c0db3ecfab
jonas.voland <jonas.voland@students.ffhs.ch>
2024-05-19 00:59:42 +0200

    Update PageHome.test.tsx

--- src/pages/PageHome.test.tsx
+++ src/pages/PageHome.test.tsx
@@ -1,11 +1,53 @@
-import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { Mock, describe, expect, it, vi } from 'vitest'
+import { useQuery } from '@tanstack/react-query'
 import PageHome from './PageHome'
 
-describe('PageHome', () => {
-  it('renders the App component', () => {
+// Mock useQuery from @tanstack/react-query
+vi.mock('@tanstack/react-query', () => ({
+  useQuery: vi.fn(),
+}))
+
+const mockUseQuery = useQuery as Mock
+
+describe('PageHome Component', () => {
+  it('should display loading state', () => {
+    mockUseQuery.mockReturnValue({
+      isPending: true,
+      error: null,
+      data: null,
+    })
+
     render(<PageHome />)
-    
-    screen.debug(); // prints out the jsx in the App component unto the command line
+
+    expect(screen.getByText('Loading...')).toBeInTheDocument()
+  })
+
+  it('should display error state', () => {
+    const errorMessage = 'An error occurred'
+    mockUseQuery.mockReturnValue({
+      isPending: false,
+      error: { message: errorMessage },
+      data: null,
+    })
+
+    render(<PageHome />)
+
+    expect(screen.getByText(`An error has occurred: ${errorMessage}`)).toBeInTheDocument()
+  })
+
+  it('should display data state', () => {
+    const mockData = { message: 'Hello, World!' }
+    mockUseQuery.mockReturnValue({
+      isPending: false,
+      error: null,
+      data: mockData,
+    })
+
+    render(<PageHome />)
+
+    expect(screen.getByText(`Hello from Page Home ${JSON.stringify(mockData)}`)).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: /button/i })).toBeInTheDocument()
   })
 })
