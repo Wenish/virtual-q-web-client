562f9094072ebba8f04dcf7a12898dee5236c896
jonas.voland <jonas.voland@students.ffhs.ch>
2024-06-03 23:38:04 +0200

    Update TheNavbar.tsx

--- src/components/TheNavbar.tsx
+++ src/components/TheNavbar.tsx
@@ -12,36 +12,37 @@ const TheNavbar = () => {
       </div>
       {isLoggedIn() && (
         <div className="flex-none">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <details className="dropdown dropdown-end">
-                <summary>{user?.username}</summary>
-                <ul className="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow">
-                  <li>
-                    <Link to="/me/tickets">My Tickets</Link>
-                  </li>
-                  <li>
-                    <Link to="/me/queues">My Queues</Link>
-                  </li>
-                  <li>
-                    <button onClick={logout}>Logout</button>
-                  </li>
-                </ul>
-              </details>
-            </li>
-          </ul>
           <div className="dropdown dropdown-end">
             <div
               tabIndex={0}
               role="button"
-              className="avatar btn btn-circle btn-ghost"
+              className="btn btn-circle btn-ghost"
             >
-              <div className="w-10">jonas</div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 scale-x-[-1]"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
+              </svg>
             </div>
             <ul
               tabIndex={0}
               className="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow"
             >
+              <div className="break-all px-3">
+                <div>
+                  Hello <b>{user?.username}</b>
+                </div>
+              </div>
+              <hr className="my-2" />
               <li>
                 <Link to="/me/tickets">My Tickets</Link>
               </li>
